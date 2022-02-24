1faa41be3e0834f836f7451a41f56d368e29a53b
scipius02 <heltonv@bu.edu>
2022-02-23 19:23:34 -1000

    Added vue learning materials

---
+++ MDN-learning-area/vue/moz-todo-vue/src/components/ToDoForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form v-on:submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input">
+        What needs to be done?
+      </label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg" />
+
+    <button type="submit" class="btn btn__primary btn__lg">
+      Add
+    </button>
+
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+        onSubmit() {
+          if(this.label === "") {
+            return;
+          }
+          this.$emit("todo-added", this.label);   // Vue templates cannot listen for events named with capitals
+          console.log("New item: ", this.label)
+          this.label = "";
+        }
+    },
+    data() {
+      return {
+        label: ""
+      };
+    }
+  }
+
+</script>
